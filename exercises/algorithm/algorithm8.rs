af50bbf4cf4eaa673c1d2e3a56d4561e4faf31e7
Ben Guo <benx.guo@gmail.com>
2025-09-17 00:38:20 +0800

    update

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,8 +2,6 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
-
 #[derive(Debug)]
 pub struct Queue<T> {
     elements: Vec<T>,
@@ -54,28 +52,46 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
 	q1:Queue<T>,
 	q2:Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
 			q1:Queue::<T>::new(),
 			q2:Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        match self.is_empty() {
+            true => self.q1.enqueue(elem),
+            false => match self.q1.is_empty() {
+                true => self.q2.enqueue(elem),
+                false => self.q1.enqueue(elem),
+            }
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        match self.is_empty() {
+            true => Err("Stack is empty"),
+            false => match self.q1.is_empty() {
+                true => {
+                    while self.q2.size() > 1 {
+                        self.q1.enqueue(self.q2.dequeue().unwrap());
+                    }
+                    self.q2.dequeue()
+                },
+                false => {
+                    while self.q1.size() > 1 {
+                        self.q2.enqueue(self.q1.dequeue().unwrap());
+                    }
+                    self.q1.dequeue()
+                }
+            }
+        }
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        self.q1.is_empty() && self.q2.is_empty()
     }
 }
 
--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,8 +2,6 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
-
 use std::cmp::Ord;
 use std::default::Default;
 
@@ -37,7 +35,39 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.items.push(value);
+        self.count += 1;
+        self.bubble_up(self.count);
+    }
+    
+    fn bubble_up(&mut self, mut idx: usize) {
+        while idx > 1 {
+            let parent_idx = self.parent_idx(idx);
+            let current = &self.items[idx];
+            let parent = &self.items[parent_idx];
+            match (self.comparator)(current, parent) {
+                true => {
+                    self.items.swap(idx, parent_idx);
+                    idx = parent_idx;
+                }
+                false => break,
+            }
+        }
+    }
+    
+    fn bubble_down(&mut self, mut idx: usize) {
+        while self.children_present(idx) {
+            let smallest_child = self.smallest_child_idx(idx);
+            let current = &self.items[idx];
+            let smallest = &self.items[smallest_child];
+            match (self.comparator)(smallest, current) {
+                true => {
+                    self.items.swap(idx, smallest_child);
+                    idx = smallest_child;
+                }
+                false => break,
+            }
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +87,12 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+        match right > self.count || (self.comparator)(&self.items[left], &self.items[right]) {
+            true => left,
+            false => right,
+        }
     }
 }
 
@@ -84,8 +118,18 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        if self.count == 0 {
+            return None;
+        }
+        
+        let result = self.items.swap_remove(1);
+        self.count -= 1;
+        
+        if self.count > 0 {
+            self.bubble_down(1);
+        }
+        
+        Some(result)
     }
 }
 
